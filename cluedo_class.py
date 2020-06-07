5498c978704b26717c7062fdd4f713bdb166213d
Dewi Gould <dewi.gould@projectpocket.co.uk>
2020-06-07 14:39:47 +0100

    updates to turn suggestion

--- cluedo_class.py
+++ cluedo_class.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from collections import Counter
+import numpy as np
 
 class CluedoGame:
 
@@ -15,9 +16,10 @@ class CluedoGame:
     
         self.all_names = other_player_names+[user_name]
         self.num_players = len(self.all_names)
+        self.set_up_board_tracker(user_name, user_cards, other_player_names)
 
 
-        # Setting up the board tracker
+    def set_up_board_tracker(self, user_name, user_cards, other_player_names):
         room_tracker = pd.DataFrame(zip(CluedoGame.rooms, ['room']*CluedoGame.num_rooms),columns=['cards','card_type'])
         guest_tracker = pd.DataFrame(zip(CluedoGame.guests, ['guest']*CluedoGame.num_guests),columns=['cards','card_type'])
         weapon_tracker = pd.DataFrame(zip(CluedoGame.weapons, ['weapon']*CluedoGame.num_weapons),columns=['cards','card_type'])  
@@ -36,60 +38,60 @@ class CluedoGame:
         self.hedges = {i:[] for i in self.all_names}
 
         self.add_definite_cards(user_name, user_cards)
+
         cards_user_doesnt_have = [i for i in CluedoGame.guests if i not in user_cards] + \
                                  [i for i in CluedoGame.rooms if i not in user_cards] + \
                                  [i for i in CluedoGame.weapons if i not in user_cards]
+        
         self.doesnt_have_cards(user_name,cards_user_doesnt_have)
 
     
     def add_definite_cards(self, name, cards):
         for card in cards:
             self.board_tracker.at[card,name] = 1.0
+            self.remove_card_from_names_hedge(name, card)
             for other in self.all_names:
                 if other != name:
                     self.board_tracker.at[card, other] = 0.0
-
-                    for hedge in self.hedges[other]: 
-                        if card in hedge:
-                            hedge.remove(card)
-                        if len(hedge) == 1:
-                            self.board_tracker.at[hedge[0], other] = 1.0
-                            hedge.remove(hedge[0])
-
+                    self.remove_card_from_names_hedge(other, card)
 
     def doesnt_have_cards(self, name, cards):
         for card in cards:
             self.board_tracker.at[card,name] = 0.0
-            for hedge in self.hedges[name]: 
-                if card in hedge:
-                    hedge.remove(card)
-                if len(hedge) == 1:
-                    self.board_tracker.at[hedge[0],name] = 1.0
-                    hedge.remove(hedge[0])
-
-
+            self.remove_card_from_names_hedge(name,card)
+ 
     def add_options(self, name, cards):
         priors = [self.board_tracker.at[card,name] for card in cards]
         unknowns = [card for card in cards if self.board_tracker.at[card, name] == 'no_info']
         num_unknowns = priors.count("no_info")
-
         if num_unknowns >0 and 1.0 not in priors:
             self.hedges[name].append(unknowns)
 
+    def remove_card_from_names_hedge(self, name, card):
+        for hedge in self.hedges[name]:
+            if card in hedge:
+                hedge.remove(card)
+            if len(hedge) == 1:
+                self.board_tracker.at[hedge[0],name] = 1.0
+                hedge.remove(hedge[0])
+
     def my_turn_suggestion(self):
+
+        if len(self.check_for_certainties()[1]) == 3:
+            return "Make accusation", self.check_for_certainties()[1]
+         
         suggestions = []
 
-        fifty_fifties = []
+        options = []
         all_hedges = []
         for hedges in self.hedges.values():
             all_hedges += hedges
         for hedges in all_hedges:
-            if len(hedges) == 2:
-                fifty_fifties.append(hedges[0])
-                fifty_fifties.append(hedges[1])
-        options = Counter(fifty_fifties).most_common()
+            options += hedges
+        options = Counter(options).most_common()
+        
         if len(options) == 0:
-            return "No suggestions yet"
+            return "Just guess a random selection of cards you don't have"
 
         for cards in [CluedoGame.rooms, CluedoGame.weapons, CluedoGame.guests]:
             found = False
@@ -100,18 +102,24 @@ class CluedoGame:
                     found = True
                 else:
                     count +=1
+        if len(suggestions) == 2:
+            return "Go for these two & a random selection out of your knowns, or if you don't have any..the unkowns", suggestions
         return suggestions
 
+
+
     def check_for_certainties(self):
+        output = []
         for card_type in ['room','weapon','guest']:
             subset = self.board_tracker.loc[self.board_tracker['card_type'] == card_type][self.all_names]
             m1 = subset.eq(0).all(axis=1)
             if True in m1.values:
                 answer = m1[m1==True].index[0]
-                print("Current Results: ", card_type, "is", answer)
+                output.append(answer)
+        return "Answers are:", output
 
     def see_progress(self):
-        self.check_for_certainties()
+        print(self.check_for_certainties())
         return self.board_tracker
 
 
